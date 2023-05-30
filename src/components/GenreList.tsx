82afc950f5a06802be08aff1d69f4cfa7a4baf6e
Harsh2972004 <harshpreet2972004@gmail.com>
2023-05-30 20:37:47 +0530

    Showing skeletons genre while fetching genres data

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,26 +1,35 @@
 import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
 import croppedImageurl from "../services/imageUrl";
+import SideBarSkeleton from "./sideBarSkeleton";
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  const skeletonsGenre = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  if (error) return null;
+
   return (
-    <ul>
-      {data.map((genre) => (
-        <List key={genre.id}>
-          <ListItem paddingY="5px">
-            <HStack>
-              <Image
-                boxSize="32px"
-                borderRadius={8}
-                src={croppedImageurl(genre.image_background)}
-              />
-              <Text fontSize="lg">{genre.name}</Text>
-            </HStack>
+    <List>
+      {isLoading &&
+        skeletonsGenre.map((skeleton) => (
+          <ListItem key={skeleton} paddingY="5px">
+            <SideBarSkeleton />
           </ListItem>
-        </List>
+        ))}
+      {data.map((genre) => (
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={croppedImageurl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
 
---
+++ src/components/SideBarSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Card, Skeleton } from "@chakra-ui/react";
+
+const SideBarSkeleton = () => {
+  return (
+    <Card>
+      <Skeleton height="40px" />
+    </Card>
+  );
+};
+
+export default SideBarSkeleton;
